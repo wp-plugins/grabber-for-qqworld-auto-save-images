12938ce4986f0e49cecde5d1dfa4a5d76eb86d40
qqworld <qqworld@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-07 01:48:20 +0000

    update 1.0
    git-svn-id: https://plugins.svn.wordpress.org/grabber-for-qqworld-auto-save-images/trunk@1084234 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ grabber-4-qasi.php
@@ -0,0 +1,158 @@
+<?php 
+/*
+Plugin Name: Grabber for QQWorld Auto Save Images
+Plugin URI: https://wordpress.org/plugins/grabber-4-qasi/
+Description: Additional grabber for QQWrorld Auto Save Images.
+Version: 1.0
+Author: Michael Wang
+Author URI: http://www.qqworld.org
+Text Domain: grabber_4_qasi
+*/
+define('GRABBER_FOR_QQWORLD_AUTO_SAVE_IMAGES_DIR', __DIR__ . DIRECTORY_SEPARATOR);
+define('GRABBER_FOR_QQWORLD_AUTO_SAVE_IMAGES_URL', plugin_dir_url(__FILE__));
+
+class Grabber_for_QQWorld_auto_save_images {
+	var $text_domain = 'grabber_4_qasi';
+	var $grab_pdf;
+	public function __construct() {
+		$grabber = get_option( 'qqworld-auto-save-images-grabber' );
+		$this->grab_pdf = isset($grabber['pdf']) ? $grabber['pdf'] : 'yes';
+		add_action( 'plugins_loaded', array($this, 'load_language') );
+		add_filter( 'plugin_row_meta', array($this, 'registerPluginLinks'),10,2 );
+		add_action( 'admin_init', array($this, 'register_settings') );
+		add_filter( 'qqworld-auto-save-images-content-save-pre', array($this, 'content_save_pre'), 10, 2 );
+		add_action( 'qqworld-auto-save-images-general-options-form', array($this, 'general_options_form') );
+	}
+
+	public function load_language() {
+		load_plugin_textdomain( 'grabber_4_qasi', dirname( __FILE__ ) . '/lang' . 'lang', basename( dirname( __FILE__ ) ) . '/lang' );
+	}
+
+	public function registerPluginLinks($links, $file) {
+		$base = plugin_basename(__FILE__);
+		if ($file == $base) {
+			$links[] = '<a href="' . menu_page_url( 'qqworld-auto-save-images', 0 ) . '">' . __('Settings') . '</a>';
+		}
+		return $links;
+	}
+
+	function register_settings() {
+		register_setting("qqworld_auto_save_images_settings", 'qqworld-auto-save-images-grabber');
+	}
+
+	public function outside_language() {
+		__( 'Michael Wang', $this->text_domain );
+		__( 'Grabber for QQWorld Auto Save Images', $this->text_domain );
+		__( 'Additional grabber for QQWrorld Auto Save Images.', $this->text_domain );
+	}
+
+	public function get_filename($url, $type) {
+		$url = explode('/', $url);
+		$filename = $url[count($url)-1];
+		if (strstr($filename, '.') > -1) {
+			$filename = explode('.', $filename);
+			$filename = $filename[count($filename)-1] . '.' . $type;
+		} else {
+			$filename = md5($filename) . '.' . $type;
+		}
+		return $filename;
+	}
+
+	public function download($url) {
+		set_time_limit(0);
+		$file = '';
+		if (function_exists('file_get_contents')) {
+			$file = @file_get_contents($url);
+			$is_pdf = false;
+			foreach ($http_response_header as $header) {
+				if ( preg_match('/Content-Type: application\/pdf/i', $header, $matches) ) {
+					$is_pdf = true;
+					$filename = $this->get_filename($url, 'pdf');
+				}
+			}
+		}
+		return $is_pdf ? array('filename' => $filename, 'file' => $file) : '';
+	}
+
+	public function content_save_pre($content, $post_id) {
+		set_time_limit(0);
+		if ( preg_match_all('/<a[^>]*href=\"(.*?)\".*?>/i', $content, $matches) ) {
+			foreach ($matches[1] as $match) {
+				$file = $this->download($match);
+				if ( !empty($file) && $res = $this->save($file['filename'], $file['file'], $post_id) ) {
+					$content = $this->format($match, $res, $content);
+				}
+			}
+		}
+		return $content;
+	}
+
+	public function encode_pattern($str) {
+		$str = str_replace('(', '\(', $str);
+		$str = str_replace(')', '\)', $str);
+		$str = str_replace('+', '\+', $str);
+		$str = str_replace('.', '\.', $str);
+		$str = str_replace('?', '\?', $str);
+		$str = str_replace('*', '\*', $str);
+		$str = str_replace('/', '\/', $str);
+		$str = str_replace('^', '\^', $str);
+		$str = str_replace('$', '\$', $str);
+		$str = str_replace('|', '\|', $str);
+		return $str;
+	}
+
+	public function format($url, $res, $content) {
+		$pattern_url = $this->encode_pattern($url);
+		$replace = $res['url'];
+		$content = preg_replace('/'.$pattern_url.'/i', $replace, $content);
+		return $content;
+	}
+
+	//insert attachment
+	public function insert_attachment($file, $parent){
+		$dirs = wp_upload_dir();
+		$filetype = wp_check_filetype($file);
+		$attachment = array(
+			'guid' => $dirs['baseurl'].'/'._wp_relative_upload_path($file),
+			'post_mime_type' => $filetype['type'],
+			'post_title' => preg_replace('/\.[^.]+$/','',basename($file)),
+			'post_content' => '',
+			'post_status' => 'inherit'
+		);
+		$attach_id = wp_insert_attachment($attachment, $file, $parent);
+		$attach_data = wp_generate_attachment_metadata($attach_id, $file);
+		wp_update_attachment_metadata($attach_id, $attach_data);
+		return $attach_id;
+	}
+
+	public function save($filename, $file, $post_id) {
+		set_time_limit(0);
+		$res = wp_upload_bits($filename, null, $file);
+		if (isset( $res['error'] ) && !empty($res['error'])) return false;
+		$attachment_id = $this->insert_attachment($res['file'], $post_id);
+		$res['id'] = $attachment_id;
+		$meta_data = wp_get_attachment_metadata($attachment_id);
+		if (!empty($meta_data)) $res = array_merge($res, $meta_data);
+		return $res;
+	}
+
+	public function general_options_form() {
+?>
+	<h2><?php _e('Additional Grabber Options', $this->text_domain); ?></h2>
+	<table class="form-table">
+		<tbody>
+			<tr valign="top">
+				<th scope="row"><label><?php _e('Grab PDF', $this->text_domain); ?></label></th>
+				<td><fieldset>
+					<legend class="screen-reader-text"><span><?php _e('Grab PDF', $this->text_domain); ?></span></legend>
+						<label for="qqworld_auto_save_images_grab_pdf">
+							<input name="qqworld-auto-save-images-grabber[pdf]" type="checkbox" id="qqworld_auto_save_images_grab_pdf" value="yes" <?php checked('yes', $this->grab_pdf); ?> />
+						</label>
+				</fieldset></td>
+			</tr>
+		</tbody>
+	</table>
+<?php
+	}
+}
+new Grabber_for_QQWorld_auto_save_images;
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Grabber for QQWorld Auto Save Images ===
+Contributors:
+Donate link:
+Tags: auto, save, local, fetch, pdf
+Requires at least: 3.5
+Tested up to: 4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Additional grabber for QQWrorld Auto Save Images. QQWorld自动保存图片的额外抓取工具。
+
+== Description ==
+
+Additional grabber <a href="https://wordpress.org/plugins/qqworld-auto-save-images/" target="_blank">for QQWrorld Auto Save Images</a>. QQWorld自动保存图片的额外抓取工具。
+
+== Installation ==
+
+<ol>
+<li>Make sure the server configuration <strong>allow_url_fopen=1</strong> in php.ini.<br />
+请确保php.ini的设置中 <strong>allow_url_fopen=1</strong></li>
+</ol>
+
+== Screenshots ==
+
+1. User interface - Settings
+2. 用户界面 - 设置
+
+== Changelog ==
+
+= 1.0 =
+New feature<br />
+Grab PDF to local media labrary.<br />
+新特性<br />
+抓取PDF到本地媒体库。
